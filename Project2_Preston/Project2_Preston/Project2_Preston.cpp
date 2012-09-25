eb6afb6a5d41feb33657b02d43000d51ee4a1e6a
danny preston <floridadlp@tampabay.rr.com>
2012-09-24 22:23:10 -0400

    Final

--- Project2_Preston/Project2_Preston/Project2_Preston.cpp
+++ Project2_Preston/Project2_Preston/Project2_Preston.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cmath>
 #include <stdlib.h>
+#include <string.h>
 #include <Windows.h>
 using namespace std;
 
@@ -14,10 +15,10 @@ int main(void)
 	int L1;
 	int L2;
 	int L3;
-	int Lucas;
-	int option;
+	char option;
 	int num;
 	
+	while(boolean exits = true){
 	cout <<"Lucas Numbers Calculator" << endl;
 	cout <<"Please enter one of the following choices."<< endl;
 	cout <<"1. To find if a number belongs to the series or not." << endl;
@@ -27,15 +28,18 @@ int main(void)
 	cin >> option;
 	cout << endl;
 	switch(option) {
-		case 1: cout <<"Please give the number you want to check." << endl;
+		case '1':{ cout <<"Please give the number you want to check.";
 				cin >> num;
 				L1=2;
 				L2=1;
-				if (num==2){
-					cout << num << " IS a Lucas number" << endl;
+				if(num <=0){
+				cout << "Only positive numbers are accepted." << endl;
+				}
+				else if (num==2){
+					cout << num << " IS a Lucas number " << endl;
 				}
 				else if (num==1){
-					cout << num << " IS a Lucas number" << endl;
+					cout << num << " IS a Lucas number " << endl;
 				}
 				else {
 					while(boolean found = false){
@@ -53,14 +57,18 @@ int main(void)
 						
 					}
 				}
-		
-		case 2: cout <<"Please give the rank of the number you are looking for: " <<endl;
+			   }
+				 break;
+		case '2': {cout <<"Please give the rank of the number you are looking for: ";
 				cin >> n;
 				int fnum;
 				L1=2;
 				L2=1;
-				if(n == 1){
-                   fnum=L1;
+				if(n <= 0){
+					cout << "Only positive numbers are accepted." << endl;
+				}
+				else if(n == 1){
+					fnum=L1;
 				}
 				else if(n == 2){
 					fnum=L2;
@@ -76,25 +84,64 @@ int main(void)
 				string ending;
 				int onesdigit = fnum % 10;
 				if(onesdigit == 1){
-					ending = "st";
+					cout << fnum<< " is the " << n << "-"<< "st" << " Lucas number.\n" << endl;
 				}
 				else if(onesdigit == 2){
-					ending = "nd";
-				}
+					cout << fnum<< " is the " << n << "-"<< "nd" << " Lucas number.\n" << endl;
+				} 
 				else if(onesdigit == 3){
-					ending = "rd";
+					cout << fnum<< " is the " << n << "-"<< "rd" << " Lucas number.\n" << endl;
 				}
 				else{
-					ending = "th";
+					cout << fnum<< " is the " << n << "-"<< "th" << " Lucas number.\n" << endl;
 				}
 
-				cout << fnum<< " is the " << &ending << "- Lucas number." << endl;
-		
-		case 3: cout <<"Please enter the upper bound of the summation you are looking for: " <<endl;
-				cin >> num;
-
-
-
 				
+				}
+				  break;
+		case '3': cout <<"Please enter the upper bound of the summation you are looking for: ";
+				cin >> num;
+				L1=2;
+				L2=1;
+				L3=L2+L1;
+				int total;
+				if(num <= 0){
+					cout << "Only positive numbers are accepted." << endl;
+				}
+				else if(num == 1){
+					total=L1+L2;
+				}
+				else if(num == 2){
+					total=L1;
+				}
+				else{
+					total=3;
+				while(boolean sum = true){
+					if (L3 > num){
+						break;
+					}
+					else {
+					 L3=L1+L2;
+				 	 L1=L2;
+					 L2=L3;
+				     total= total+L3;
+					}
+			    }
+				}
+				cout << total << " is the summation of all Lucas numbers until " << num << endl;
 
+				break;
+		case 'q':{
+			     cout << "Now quitting." << endl;
+				 exits = false;
+				 exit(0);
+				 }
 
+		default:{
+				cout << "Wrong choice. Please choose again." << endl;
+				}
+				
+		}
+		
+	}
+	}
